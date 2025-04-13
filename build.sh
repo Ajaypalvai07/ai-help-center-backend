db3699fe9e37ea83c1cf9df4cb3b80f93550ea87
Ajay Palvai <palvaiajay4730@gmail.com>
2025-04-13 20:44:24 +0530

    commit some updates

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 # Exit on error
 set -e
 
+# Set Python version
+export PYTHON_VERSION=3.11.7
+
+echo "Setting up Python ${PYTHON_VERSION}..."
+pyenv install ${PYTHON_VERSION} -s || true
+pyenv global ${PYTHON_VERSION}
+
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
